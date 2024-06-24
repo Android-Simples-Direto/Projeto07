315db905ae6f558255fd4a61b17ddd84a2539e78
Sidnei-1975-Almeida <81591660+Sidnei-1975-Almeida@users.noreply.github.com>
2024-06-24 17:27:23 -0300

    Projeto07

---
+++ app/src/main/java/com/projeto07/projeto07/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.projeto07.projeto07
+
+import android.content.Intent
+import android.graphics.Color
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnIniciar: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        btnIniciar = findViewById(R.id.btn_PrimeiraTela)
+
+        btnIniciar.setOnClickListener {
+
+            val iniciar = Intent(this, SegundaTela::class.java)
+
+            startActivity(iniciar)
+
+        }
+
+        window.statusBarColor = Color.parseColor("#8A19D6")
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
